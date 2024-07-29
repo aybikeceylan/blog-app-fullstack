02bed96df9cc4bdd0a8b5b6795f075ae40cd861e
Aybike <96443704+aybikeceylan@users.noreply.github.com>
2024-07-29 14:06:26 +0300

    intial commit

---
+++ app/Components/Header.jsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import React from "react";
+import Logo from "../Assets/connection.png";
+import Arrow from "../Assets/greenArrow.jpeg";
+
+const Header = () => {
+  return (
+    <div className="py-5 px-5 md:px-12 lg:px-28">
+      <div className="flex justify-between items-center">
+        <Image
+          src={Logo}
+          width={120}
+          height={120}
+          alt="logo"
+          className="w-[130px] sm:w-auto"
+        />
+        <button className="flex items-center gap-2 font-medium py-1 px-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]">
+          {" "}
+          Get Started{" "}
+          <Image
+            src={Arrow}
+            width={30}
+            height={30}
+            alt="arrow"
+            className="w-[130px] sm:w-auto"
+          />
+        </button>
+      </div>
+      <div className="text-center my-8">
+        <h1 className="text-3xl sm:text-5xl font-medium">Latest Blogs</h1>
+        <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <form className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="pl-4 outline-none"
+          />
+          <button
+            type="submit"
+            className="border-l border-black py-4 px-4 sm:px-8 active:bg-gray-800 active:text-white"
+          >
+            Subscribe
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
